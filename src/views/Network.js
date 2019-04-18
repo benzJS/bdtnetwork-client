7b600a101eef61db0c571c8fceb0487ba67782d0
benznt <tmno2tm@gmail.com>
2019-04-18 20:26:33 +0700

    features/add-network

--- src/views/Network.js
+++ src/views/Network.js
@@ -4,7 +4,25 @@ import React, { useState, useEffect } from "react";
 // import paginationFactory from "react-bootstrap-table2-paginator";
 // import cellEditFactory, { Type } from 'react-bootstrap-table2-editor';
 import ReactTable, { ReactTableDefaults } from "react-table";
-import { Container, InputGroup, InputGroupAddon, Button, Input, Table } from 'reactstrap';
+import {
+    Alert,
+    Container,
+    InputGroup,
+    InputGroupAddon,
+    Button,
+    Input,
+    Row,
+    Col,
+    Form,
+    FormGroup,
+    Label,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
+} from 'reactstrap';
+import { Formik } from 'formik';
+import axios from 'axios';
 
 // import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import 'react-table/react-table.css'
@@ -22,13 +40,14 @@ export default props => {
     const copyToClipboard = text => {
         navigator.clipboard.writeText(text);
     }
-    const getColumns = (id = -1) => {
+    const getColumns = () => {
         const width = window.innerWidth;
         const numberOfCol = width >= 1200 ? 8 : width >= 992 ? 6 : width >= 768 ? 4 : 3;
         return Object.keys(data[0]).slice(0, numberOfCol).map(key => {
             const column = {
                 Header: key,
-                accessor: key
+                accessor: key,
+                Cell: props => `${props.value}`
             }
             key === 'first_name' && (column.Cell = props =>
                 {
@@ -49,7 +68,8 @@ export default props => {
         columns: [],
         progressValue: 20,
         inputRef: null,
-        isMenuOpen: false
+        isMenuOpen: false,
+        modal: false
     })
     const SubComponent = row => {
         const missingCol = Object.keys(data[0]).filter(
@@ -68,6 +88,14 @@ export default props => {
                 }
             </ul>
         );
+    }
+    const toggle = () => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                modal: !prevState.modal
+            }
+        })
     }
     useEffect(() => {
         setState(prevState => {
@@ -88,7 +116,15 @@ export default props => {
     return (
         <Container>
             <div className="menu-bar">
-                <Button color="primary">Add new item</Button>
+                <Row>
+                    <Col md="6">
+                        <Button color="info" className="mr-3" onClick={toggle}>Add new item</Button>
+                        <Button color="danger">Delete multiples</Button>
+                    </Col>
+                    <Col md="6">
+                        <Input placeholder="search..." />
+                    </Col>
+                </Row>
             </div>
             <ReactTable
                 // TableComponent={props => <table {...props}>{props.children}</table>}
@@ -103,6 +139,71 @@ export default props => {
                 className="-striped"
                 defaultPageSize={10}
                 SubComponent={Object.keys(data[0]).length > state.columns.length ? SubComponent : null}
+            />
+            <Formik
+                initialValues={{ name: '', type: 'banner', postback: 'https://api.bdtnetworks.com/banner/BNaHmwWryUCS8siBhiNnA/{sub_id}', response: 1 }}
+                onSubmit={(values, actions) => {
+                    // setTimeout(() => {
+                    //     alert(JSON.stringify(values, null, 2));
+                    //     actions.setSubmitting(false);
+                    // }, 1000);
+                    axios.post('http://localhost:5000/network', values)
+                        .then(res => {
+                            alert(JSON.stringify(res.data.data, null, 2));
+                            actions.setSubmitting(false);
+                            return;
+                        }).catch(err => {
+                            console.log(err);
+                            actions.setErrors({global: 'Failed'});
+                            actions.setSubmitting(false);
+                        });
+                }}
+                render={props => {return (
+                    <Modal isOpen={state.modal} toggle={toggle} >
+                        <ModalHeader toggle={toggle}>Add new item</ModalHeader>
+                        <Form onSubmit={props.handleSubmit}>
+                            <ModalBody>
+                                {props.isValid.global && <Alert color="danger">{props.errors.global}</Alert>}
+                                <FormGroup row>
+                                    <Label for="exampleEmail" sm={2}>Name</Label>
+                                    <Col sm={10}>
+                                        <Input type="text" name="name" placeholder="Network" value={props.values.name} onChange={props.handleChange} />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label for="exampleSelect" sm={2}>Type</Label>
+                                    <Col sm={10}>
+                                        <Input type="select" name="type" value={props.values.type} onChange={props.handleChange} >
+                                            <option value="banner">Banner</option>
+                                            <option value="wall">Wall</option>
+                                        </Input>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label for="examplePassword" sm={2}>Postback</Label>
+                                    <Col sm={10}>
+                                        <InputGroup>
+                                            <Input type="text" name="postback" value={props.values.postback} onChange={props.handleChange} />
+                                            <InputGroupAddon addonType="append">
+                                                <Button style={{zIndex: 1}} onClick={copyToClipboard.bind(this, props.values.postback)}>Copy</Button>
+                                            </InputGroupAddon>
+                                        </InputGroup>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label for="examplePassword" sm={2}>Response</Label>
+                                    <Col sm={10}>
+                                        <Input type="text" name="response" placeholder="Response" value={props.values.response} onChange={props.handleChange} />
+                                    </Col>
+                                </FormGroup>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button type="submit" color="info" disabled={props.isSubmitting} >Submit</Button>{' '}
+                                <Button color="secondary" onClick={toggle} disabled={props.isSubmitting} >Cancel</Button>
+                            </ModalFooter>
+                        </Form>
+                    </Modal>
+                )}}
             />
         </Container>
     )
